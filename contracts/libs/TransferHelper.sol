e3b89542f7cfdc167c35359ba635c71333513b9c
Max <max@MaxdeMacBook-Pro.local>
2023-06-21 16:52:57 +0800

    fix TransferHelper

--- contracts/libs/TransferHelper.sol
+++ contracts/libs/TransferHelper.sol
@@ -22,6 +22,12 @@ library TransferHelper {
         require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: TRANSFER_TOKEN_FAILED');
     }
 
+    function safeTransferWithoutRequire(address token, address to, uint256 value) internal returns (bool) {
+        // bytes4(keccak256(bytes('transfer(address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
+        return (success && (data.length == 0 || abi.decode(data, (bool))));
+    }
+
     function safeTransferFrom(address token, address from, address to, uint value) internal {
         // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
